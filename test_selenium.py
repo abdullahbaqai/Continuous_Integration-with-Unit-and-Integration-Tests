f7c4ae15131ff8c844869474a8990427253bf19f
abdullahbaqai <baqaiabdullah5@gmail.com>
2025-05-07 21:42:44 +0500

    a

--- test_selenium.py
+++ test_selenium.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.binary_location = "/usr/bin/chromium-browser"  # or "/usr/bin/chromium"
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")  # Run in headless mode
+chrome_options.add_argument("--no-sandbox")  # Bypass the sandbox for CI environments
+chrome_options.add_argument("--disable-dev-shm-usage")  # Disable /dev/shm usage
+chrome_options.add_argument("--remote-debugging-port=9222")  # Use remote debugging port
+chrome_options.add_argument("--disable-gpu")  # Disable GPU hardware acceleration (if needed)
 
 driver = webdriver.Chrome(options=chrome_options)
