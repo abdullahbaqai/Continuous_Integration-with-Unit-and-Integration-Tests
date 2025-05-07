8ee494888079dc43554f2d8f5df2ae2477a7d193
abdullahbaqai <baqaiabdullah5@gmail.com>
2025-05-07 21:40:51 +0500

    a

--- test_selenium.py
+++ test_selenium.py
@@ -1,25 +1,8 @@
-import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
 
-# Configure options
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.binary_location = "/usr/bin/chromium-browser"  # or "/usr/bin/chromium"
 
-# Use environment variable
-options.binary_location = os.getenv("CHROME_BIN", "/usr/bin/chromium-browser")
-
-# Create WebDriver instance
-driver = webdriver.Chrome(options=options)
-
-# Run test
-driver.get("http://127.0.0.1:5000")
-time.sleep(2)
-heading = driver.find_element(By.TAG_NAME, "h1")
-assert "Welcome to Flask!" in heading.text
-print("Test Passed!")
-driver.quit()
+driver = webdriver.Chrome(options=chrome_options)
